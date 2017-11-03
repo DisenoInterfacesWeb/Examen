043fe6949947efb96fecb1c82323b439a410811c
Ray <info@railtoncardoso.com>
2017-11-03 11:00:05 +0100

    fsadff

--- PracticaFinalJS/WebContent/js/codigo.js
+++ PracticaFinalJS/WebContent/js/codigo.js
@@ -1,15 +1,15 @@
 
 function CalcularValores() {
     var valLapis = document.getElementById('nLapis').value * 2.5;
-    document.getElementById('textLapis').innerHTML = "  " + valLapis;
+    document.getElementById('textLapis').innerHTML = "  " + valLapis.toFixed(2);
 
     var valRotu = document.getElementById('nRotu').value * 3.2;
-    document.getElementById('textRotu').innerHTML = "  " + valRotu;
+    document.getElementById('textRotu').innerHTML = "  " + valRotu.toFixed(2);
 
     var valLibr = document.getElementById('nLibr').value * 5.4;
-    document.getElementById('textLibr').innerHTML = "  " + valLibr;
+    document.getElementById('textLibr').innerHTML = "  " + valLibr.toFixed(2);
 
-    document.getElementById('mensaje').innerHTML = "Valor total da compra: " + (valLapis + valRotu + valLibr);
+    document.getElementById('mensaje').innerHTML = "Valor total da compra: " + (valLapis + valRotu + valLibr).toFixed(2);
 }
 
 function BorrarValores() {
